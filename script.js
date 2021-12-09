f57b11d3ce90ebebfad0ae85aaefe9e201b5208c
elena-pak <alena.pak2000@gmail.com>
2021-12-09 19:54:28 +0600

    task0019

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-alert("hello world!")
-console.log("h3110 w0r1D!")
-let a = "99"
-let name = "elena"
-console.log(name)
+// //всплывающее окно
+// alert("hello world!")
+// //вывод в консоли
+// console.log("h3110 w0r1D!")
+// //создаем переменную
+// let a = "99"
+// let name = "elena"
+// //вывод значения переменной
+// console.log(name)
+
+// //во всплывающем окне вводим значение и выводим в консоль
+// let myName = prompt("enter your name")
+// console.log(myName)
+
+// //подсчитывание возраста 
+// let year = prompt('enter your birthday')
+// let age = 2021 - year //string
+// console.log(age)
+// //подсчитывание возраста
+// let year = prompt('enter your birthday')
+// let age = 2021 - Number(year) //number
+// //вывод ответа в консоли
+// console.log('your age is ' + String(age) + ' y.o')
+
+// //периметр прямоугольника
+// //ввести стороны
+// let a = Number(prompt('side 1:'))
+// let b = Number(prompt('side 2:'))
+// //вывести периметр
+// let p = 2 * (a + b)
+// console.log(p)
+
+// //радиус круга
+// let r = Number(prompt('radius:'))
+// let s = 3.14 * (r ** 2)
+// console.log(s)
+
+// //след новое значение увелич на 1
+// let n = 7
+// n = n + 1 //n += 1
+// alert(n)
+
+// let m = 7
+// m++
+// console.log(m)
+
+//homework2
+//wallpapers
+let h = Number(prompt('Wall height(m):'))
+let a = Number(prompt('Wall weight(m):'))
+let s = h * a
+console.log('Wall Square is ' + s + ' m2')
